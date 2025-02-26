810875866755b087cb8a0987a0b156d6f0d539f7
yu-ta-website <shirohige1011@gmail.com>
2025-02-26 11:06:04 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,9 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
 btn.addEventListener('click', () => {
     console.log('ボタンをクリックしました');
     // メッセージ表示用の要素にメッセージを設定
     text.textContent = 'ボタンをクリックしました';
 });
+
